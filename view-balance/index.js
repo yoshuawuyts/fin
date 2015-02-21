9aae9d82172ee4235ef3fa1a09b11c9b68594c18
Yoshua Wuyts <i@yoshuawuyts.com>
2015-02-21 23:47:40 +0100

    client: add count widget

--- view-balance/index.js
+++ view-balance/index.js
@@ -1,7 +1,11 @@
-const xHeader = require('@finfin/x-header')
+const header = require('@finfin/x-header')
+const path = require('path')
+const fs = require('fs')
+
+// const template = fs.readFileSync(path.join(__dirname, '/index.html'), 'utf8')
 
 module.exports = view
 
+  header.render(document.body, {text: 'count is:'})
 function view() {
-  console.log(xHeader())
 }
--- x-header/index.js
+++ x-header/index.js
@@ -1,8 +1,22 @@
-
 const deku = require('deku')
+const component = deku.component
+const dom = deku.dom
 
-module.exports = header
+var button = module.exports = component()
 
-function header() {
-  
+button.prototype.initialState = function() {
+  return { count: 0 }
+}
+
+// render
+button.prototype.render = function(props, state) {
+  return dom('button', { onClick: onClick }, props.text + ' ' + state.count)
+}
+
+// onClick
+// @param {Event} e
+// @param {Object} props
+// @param {Object} state
+function onClick (e, props, state) {
+  this.setState({ count: state.count + 1 })
 }
